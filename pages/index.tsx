b95e85246a8949d212f4f0f3ba528764ffb36cd9
siliconjungle <siliconjungle.io@gmail.com>
2023-10-13 12:28:11 -0700

    change the heading

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Next.js on Replit!
+          Welcome to Silicon Jungle on Replit!
         </h1>
 
         <p className={styles.description}>
