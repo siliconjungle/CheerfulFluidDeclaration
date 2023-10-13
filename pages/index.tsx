21ccf58d37c9261c65d751f8c32129bf22dd479e
siliconjungle <71357672+siliconjungle@users.noreply.github.com>
2023-10-13 21:18:43 +0000

    remove the first paragraph

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,11 +17,6 @@ const Home: NextPage = () => {
           Welcome to Silicon Jungle on Replit!
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
